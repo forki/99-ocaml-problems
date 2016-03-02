bf628f1ef5f65de7a74e2dbabe167712dfc0400b
TIHan <lol.tihan@gmail.com>
2016-03-02 09:39:21 -0800

    Initial and MathiasWill.fsx

---
+++ MathiasWill/MathiasWill/MathiasWill.fsx
@@ -0,0 +1,28 @@
+﻿open System
+
+module ``First Problem`` =
+
+    let rec last xs =
+        match xs with
+        | [] -> None
+        | [x] -> Some(x)
+        | hd::tl -> last tl
+             
+    let test1 () =
+        Some("d") = last [ "a" ; "b" ; "c" ; "d" ]
+
+    let test2 () =
+        None = last []
+
+module ``Second Problem`` =
+
+    let rec last_two xs =
+        match xs with
+        | [] -> None
+        | [x] -> None
+        | [a;b] -> Some(a,b)
+        | hd::tl -> last_two tl             
+
+    Some("c","d") = last_two [ "a"; "b"; "c"; "d" ]
+
+    None = last_two [ "a" ]
